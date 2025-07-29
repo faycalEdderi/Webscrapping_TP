2e05df2ca8a0bf45e751fee6140b5da8bee405ed
faycalEdderi <faycmx@live.fr>
2025-07-29 16:07:22 +0200

    auto register working

--- register_user.py
+++ register_user.py
@@ -1,4 +1,3 @@
-# register_user.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
@@ -6,7 +5,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 import time
 import random
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver = webdriver.Firefox()
 
 try:
     driver.get("https://automationexercise.com/")
